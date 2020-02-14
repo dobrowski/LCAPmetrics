85de40c6e24a1e426f69633c346c793114344637
David Dobrowski <dobrowski@gmail.com>
2020-02-14 10:34:48 -0800

    added descriptions for metrics addition indicators join together

--- LCAPmetrics.R
+++ LCAPmetrics.R
@@ -11,7 +11,31 @@ library(here)
 metrics <- tibble("priority_area" = c(rep("Conditions for Learning",5),rep("Pupil Outcomes",7), rep("Engagement",10) ) ,
                   "priorities" = c(rep("Basic",3),"Standards","Access", rep("Pupil Achievement", 6), "Other Pupil Outcomes", rep("Parent Involvement",2), rep("Pupil Engagement", 5), rep("School Climate",3) ),
                   "metrics" = c("TeachersCred","Materials","GoodRepair","Standards","BroadCourse","CAASP","A_G","ELPI","reclass","AP","EAP","outcomes_other", "ParentInput","UnduplicatedParentPart","Attendance","ChronicAbs","MSdropout","HSdropout","HSgrad","susp","exp","local_other"),
-                  "source" = c(rep("dash_local",5),rep("dash",3),"dq", "dash", "dq", "local",rep("dash_local",2), "local", "dash", rep("dq",2), rep("dash",2), "dq", "dash_local"   )
+                  "source" = c(rep("dash_local",5),rep("dash",3),"dq", "dash", "dq", "local",rep("dash_local",2), "local", "dash", rep("dq",2), rep("dash",2), "dq", "dash_local"   ),
+                  "description" = c("Teachers: Fully Credentialed & Appropriately Assigned", 
+                                      "Standards-aligned Instructional Materials for every student",
+                                      "School Facilities in “Good Repair” per CDE’s Facility Inspection Tool (FIT)",
+                                      "Implementation of all CA state standards including how ELs will access the CCSS and ELD standards", 
+                                      "Students have access and are enrolled in a broad course of study (Social Science, Health, VAPA, Science, PE, World Language)",
+                                      "State CAASPP assessments (ELA, CAA, Math, Science-CST/CMA/CAPA)", 
+                                      "% of pupils that have successfully completed A-G requirements or CTE pathways (Add”l Dashboard Reports)", 
+                                      "% of ELs who progress in English proficiency (ELPAC)",
+                                      "EL reclassification rate",
+                                      "% of pupils that pass AP exams with a score of 3 or higher (Add’l Dashboard Reports)",
+                                      "Pupils prepared for college by the EAP (ELA/Math CAASPP Score of 3 or higher)",
+                                      "If available, outcomes for subjects listd in course access" ,
+                                      "Parent input in decision-making",
+                                      "Parental Participation in programs for Unduplicated Pupils (UPs)", 
+                                      "Attendance rates", 
+                                      "Chronic absenteeism (CA) rates", 
+                                      "Middle school dropout rates", 
+                                      "High school dropout rates", 
+                                      "High school graduation rates", 
+                                      "Suspension rates", 
+                                      "Expulsion rates", 
+                                      "Other local measures (Surveys re safety and school connectedness)"
+                                      )
+
 )
 
 
@@ -26,23 +50,23 @@ dashboard_mry <- dashboard_all %>%
 
 elpi <- dashboard_mry %>%
     filter(ind == "elpi") %>%
-    select(cds:countyname,currstatus)
+    select(cds:countyname, elpi = currstatus)
 
 A_G <- dashboard_mry %>%
     filter(ind == "cci",
-           studentgroup == "ALL")  %>%
-    select(cds:countyname,currstatus,currdenom ,curr_prep_agplus, curr_prep_cteplus,  curr_aprep_ag, curr_aprep_cte) %>%
+           studentgroup == "ALL") %>%
     mutate(ag_cte = curr_prep_agplus + curr_prep_cteplus + curr_aprep_ag + curr_aprep_cte,
-           ag_cte_perc = ag_cte/currdenom)
+           ag_cte_perc = ag_cte/currdenom) %>%
+    select(cds:countyname,ag_cte_perc) 
 # Note, this is the best possible calculation, but will be slightly inflated for students that completed A-G and also complete a CTE pathway. 
 # It is not possible to get the exact figure with the aggregate data available
 
 
 AP <- dashboard_mry %>%
     filter(ind == "cci",
-           studentgroup == "ALL")  %>%
-    select(cds:countyname,currstatus,currdenom , curr_prep_apexam ) %>%
-    mutate(ap = curr_prep_apexam/currdenom)
+           studentgroup == "ALL") %>%
+    mutate(ap = curr_prep_apexam/currdenom) %>%
+    select(cds:countyname,ap ) 
 # Note this is percentage of cohort that passed TWO AP exams. PErcentage that passed a single one is not available on Dashboard
 
 chronic <- dashboard_mry %>%
@@ -54,6 +78,15 @@ grad <- dashboard_mry %>%
     filter(ind == "grad",
            studentgroup == "ALL") %>%
     select(cds:countyname, grad = currstatus) 
+
+susp <- dashboard_mry %>%
+    filter(ind == "susp",
+           studentgroup == "ALL") %>%
+    select(cds:countyname, susp = currstatus) 
+
+indicators <- list(A_G, AP, chronic, elpi, grad, susp) %>%
+    reduce( left_join)
+
 
 
 ### End -----
