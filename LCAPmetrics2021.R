15cc9a32f1fb635b63e8d9009820323053f5cf28
David Dobrowski <dobrowski@gmail.com>
2021-04-23 11:14:50 -0700

    Revised Reclassification calculation to use prior year as denominator

--- LCAPmetrics2021.R
+++ LCAPmetrics2021.R
@@ -25,77 +25,84 @@ metrics <- tibble("priority_area" = c(rep("Conditions for Learning",5),rep("Pupi
                   "priorities" = c(rep("1. Basic",3),"2. Implement State Standards","7. Course Access", rep("4. Pupil Achievement*", 7), "8. Other Pupil Outcomes", rep("3. Parent Involvement",2), rep("5. Pupil Engagement*", 5), rep("6. School Climate*",3) ),
                   "metrics" = c("cred.rate.wt","Materials","GoodRepair","Standards","BroadCourse", #5
                                 "math", "ela" ,"ag_cte_perc","elpi","reclass_rate", #10
-                                "ap","EAP","outcomes_other", "ParentInput","UnduplicatedParentPart", #15
+                                "ap","eap","outcomes_other", "ParentInput","UnduplicatedParentPart", #15
                                 "Attendance","chronic","MSdropout","DropoutRate","grad", #20
                                 "susp","exp","local_other"),
                   "source" = c(rep("Local Dashboard Data",1),
                                rep("Local Dashboard Data and  district Williams Report",2),
                                "LEA and site CCSS implementation plans and teacher participation in CCSS training.",
-                               "student information systems",
-                               rep("Dashboard",4),
-                               "Dataquest",# https://www.cde.ca.gov/ci/gs/hs/eapindex.asp  doesn't exist, maybe in CCI?
+                               "student information systems", #5
+                               rep("Dashboard",2),
                                "Dashboard",
-                               "Dataquest",
+                               "Dashboard",
+                               "Dataquest",# https://www.cde.ca.gov/ci/gs/hs/eapindex.asp  doesn't exist, maybe in CCI? #10
+                               "Dashboard",
+                               "CAASPP results",
                                "Local data",
-                               rep("Local Dashboard Data and district and school surveys related to WASC and Single Plan Student Achievement, or DLAC/ELAC",1),
-                               "Local Dashboard Data and local data on parent involvement in district/school activities (e.g., committees, student clubs, after school enrichment, fundraisers, carnivals, promotion activities, PTO membership)",
+                               "Local Dashboard Data and district and school surveys related to WASC and Single Plan Student Achievement, or DLAC/ELAC",
+                               "Local Dashboard Data and local data on parent involvement in district/school activities (e.g., committees, student clubs, after school enrichment, fundraisers, carnivals, promotion activities, PTO membership)",#15
                                "student information systems",
                                "Dashboard",
                                rep("student information systems",2),
-                               rep("Dashboard",2), 
+                               "Dashboard", #20
+                               "Dataquest", 
                                "Dataquest", 
                                "Local Dashboard Data and from such sources as LEA plans, School Site Council activities, and English Learner Advisory Council materials"   ),
                   "description" = c("Teachers: Fully Credentialed & Appropriately Assigned", 
                                     "Standards-aligned Instructional Materials for every student",
                                     "School Facilities in “Good Repair” per CDE’s Facility Inspection Tool (FIT)",
                                     "Implementation of all CA state standards including how ELs will access the CCSS and ELD standards", 
-                                    "Students have access and are enrolled in a broad course of study (Social Science, Health, VAPA, Science, PE, World Language)",
+                                    "Students have access and are enrolled in a broad course of study (Social Science, Health, VAPA, Science, PE, World Language)", #5
                                     "State CAASPP assessments (Math DFS)", 
                                     "State CAASPP assessments (ELA DFS)", 
                                     "Percent of pupils that have successfully completed A-G requirements or CTE pathways", 
                                     "Percent of ELs who progress in English proficiency (ELPAC)",
-                                    "EL reclassification rate",
+                                    "EL reclassification rate", #10
                                     "Percent of pupils that pass AP exams with a score of 3 or higher",
                                     "Pupils prepared for college by the EAP (ELA/Math CAASPP Score of 3 or higher)",
                                     "If available, outcomes for subjects listed in course access" ,
                                     "Parent input in decision-making",
-                                    "Parental Participation in programs for Unduplicated Pupils (UPs)", 
+                                    "Parental Participation in programs for Unduplicated Pupils (UPs)", #15
                                     "Attendance rates", 
                                     "Chronic absenteeism (CA) rates", 
                                     "Middle school dropout rates", 
                                     "High school dropout rates", 
-                                    "High school graduation rates", 
+                                    "High school graduation rates", #20
                                     "Suspension rates", 
                                     "Expulsion rates", 
                                     "Other local measures (Surveys regarding safety and school connectedness)"
                   ),
                   "website" = c("https://www.cde.ca.gov/ds/sd/df/filesstaffdemo.asp",
-                                rep("",4),
-                                rep("https://www.cde.ca.gov/ta/ac/cm/datafiles2019.asp",4),
-                                "https://www.cde.ca.gov/ds/sd/sd/filesreclass.asp",
-                                "https://www.cde.ca.gov/ta/ac/cm/datafiles2019.asp",
-                                rep("",5),
-                                "https://www.cde.ca.gov/ta/ac/cm/datafiles2019.asp",
+                                rep("",4), #5
+                                rep("Dashboard Datafiles at https://www.cde.ca.gov/ta/ac/cm/datafiles2019.asp",2),
+                                "Dashboard Datafiles at https://www.cde.ca.gov/ta/ac/cm/",
+                                "Dashboard Datafiles at https://www.cde.ca.gov/ta/ac/cm/datafiles2019.asp",
+                                "https://www.cde.ca.gov/ds/sd/sd/filesreclass.asp", #10
+                                "Dashboard Datafiles at https://www.cde.ca.gov/ta/ac/cm/",
+                                "caaspp-elpac.cde.ca.gov",
+                                rep("",4), #16
+                                "Dashboard Datafiles at https://www.cde.ca.gov/ta/ac/cm/datafiles2019.asp",
                                 "",
-                                "https://www.cde.ca.gov/ds/sd/sd/filesfycgr.asp",
-                                rep("https://www.cde.ca.gov/ta/ac/cm/datafiles2019.asp",2),
+                                "https://www.cde.ca.gov/ds/sd/sd/filesacgr.asp",
+                                "Dashboard Datafiles at https://www.cde.ca.gov/ta/ac/cm/", #20
+                                "https://www.cde.ca.gov/ds/sd/sd/filessd.asp",
                                 "https://www.cde.ca.gov/ds/sd/sd/filesed.asp",
                                 rep("",1)
                   ),
                   "notes" = c( "Please note this only looks at teachers and not adminstrators, pupil services, itinerant nor push-in/pull-out teachers. The most recent available public data is 2018-19. It is grouped at the district level, and is weighted by percent FTE.  It does not yet look at proper assignment of teachers, only credential status. Please also reference your Williams Report."  ,
                                rep("",4), #5
-                               rep("It is recommended to use local data, such as interims or NWEA, rather than data from two years ago.",2),
-                               "Please note this is the best possible calculation, but will be slightly inflated for students that completed A-G and also complete a CTE pathway. It is not possible to get the exact figure with the aggregate data available.  CALPADS Report 3.14, 3.15 and 15.1 may be helpful.", # just use the CCI indicator too. 
+                               rep("Since this is from 2018-19, it is recommended to use local data, such as interims or NWEA, rather than data from two years ago.",2),
+                               "Note 2019-20 data is available on the Dashboard. Please note this is the best possible calculation, but will be slightly inflated for students that completed A-G and also complete a CTE pathway. It is not possible to get the exact figure with the aggregate data available.  CALPADS Report 3.14, 3.15 and 15.1 may be helpful.", # just use the CCI indicator too. 
                                rep("",2), #10  #  Look at Title III AMOA 2  reporting
-                               "Please note this is the percentage of the graduating cohort that passed TWO AP exams. The percentage of students that passed a single AP exam is not available on the Dashboard",  #  Look at AP data file,  also add link to College Board Online https://scores.collegeboard.org/pawra/home.action
-                               rep("",2),  #  Remove EAP reference to dataquest.  https://www.cde.ca.gov/ci/gs/hs/eapindex.asp
+                               "Note 2019-20 data is available on the Dashboard. Please note this is the percentage of the graduating cohort that passed TWO AP exams. The percentage of students that passed a single AP exam is not available on the Dashboard. Please also look at College Board Online https://scores.collegeboard.org/pawra/home.action",  #  Look at AP data file,  also add link to College Board Online https://scores.collegeboard.org/pawra/home.action
+                               rep("",2),  #   https://www.cde.ca.gov/ci/gs/hs/eapindex.asp
                                "One possible example is a rating on a self-assessment tool.",
                                "Consider daily participation or weekly engagement records.", #15
                                "",
                                "CALPADS Report 14.1 and 14.2 may be helpful.", #17
                                "Please note LEAs can find middle school dropouts information from CALPADS snapshot report 1.14: Dropouts Count – State View (filtered for grades seven and eight)",
                                "",
-                               "CALPADS Report 15.1 and 15.2 may be helpful.", #20
+                               "Note 2019-20 data is available on the Dashboard.  CALPADS Report 15.1 and 15.2 may be helpful.", #20
                                "CALPADS Report 7.10 and 7.12 may be helpful.",
                                "Please note this is a rate per thousand students.",
                                ""
@@ -142,62 +149,25 @@ pull.dash <- function(tablename, col){
 }
 
 
-# chronic <- tbl(con, "DASH_CHRONIC") %>%
-#     filter(studentgroup == "ALL",
-#            reportingyear == max(reportingyear)) %>%
-#     select(cds:countyname, chronic = currstatus) %>%
-#     collect()  %>%
-#     filter(str_detect(countyname,"Monterey"))
-
 
 chronic <- pull.dash("DASH_CHRONIC","chronic")
 
-# grad <- tbl(con, "DASH_GRAD") %>%
-#     filter(studentgroup == "ALL",
-#            reportingyear == max(reportingyear)) %>%
-#     select(cds:countyname, grad = currstatus) %>%
-#     collect()  %>%
-#     filter(str_detect(countyname,"Monterey"))
-
 grad <- pull.dash("DASH_GRAD","grad")
 
-
-# susp <- tbl(con, "DASH_SUSP") %>%
-#     filter(studentgroup == "ALL",
-#            reportingyear == max(reportingyear),
-#            rtype == "D") %>%  # To only get districts and because elementary and high school have suspensions this is the initial key
-#     select(cds:countyname, susp = currstatus) %>%
-#     collect()  %>%
-#     filter(str_detect(countyname,"Monterey"))
-
-
-susp <- pull.dash("DASH_SUSP","susp") %>%
-    filter( rtype == "D")
-
-
-
-
-# math <- tbl(con, "DASH_MATH") %>%
-#     filter(studentgroup == "ALL",
-#            reportingyear == max(reportingyear)) %>%
-#     select(cds:countyname, math = currstatus)  %>%
-#     collect()  %>%
-#     filter(str_detect(countyname,"Monterey"))
-
+susp <- tbl(con, "SUSP") %>%
+         filter(reporting_category == "TA",
+                academic_year == max(academic_year),
+                aggregate_level == "D",
+                charter_yn == "No") %>%  # To only get districts and because elementary and high school have suspensions this is the initial key
+         select(county_code:county_name, susp = suspension_rate_total) %>%
+         collect()  %>%
+    mutate(cds = paste0(county_code,district_code,school_code)) %>%
+         filter(str_detect(county_name,"Monterey")) %>%
+    select(cds, susp)
 
 math <- pull.dash("DASH_MATH","math")
 
-
-# ela <- tbl(con, "DASH_ELA") %>%
-#     filter(studentgroup == "ALL",
-#            reportingyear == max(reportingyear)) %>%
-#     select(cds:countyname, ela = currstatus)  %>%
-#     collect()  %>%
-#     filter(str_detect(countyname,"Monterey"))
-
-
 ela <- pull.dash("DASH_ELA","ela")
-
 
 elpi <- tbl(con, "DASH_ELPI") %>%
     select(cds:countyname, elpi = currstatus) %>%
@@ -230,6 +200,28 @@ A_G <- tbl(con, "DASH_CCI") %>%
 
 ## EAP 
 # % at level 3 on Math  ;  % at level 3 on ELA 
+
+eap.full <- tbl(con, "CAASPP") %>%
+    filter(Subgroup_ID == "1",
+           Test_Year == max(Test_Year),
+           County_Code == "27",
+           School_Code == "0000000",
+           Grade == "11") %>%  
+    select(County_Code:Test_Id, eap = Percentage_Standard_Met_and_Above) %>%
+    collect()  
+
+
+eap <- eap.full %>%
+    mutate(cds = paste0(County_Code,District_Code,School_Code),
+           eap = as.numeric(eap)
+           ) %>%
+    select(cds, Test_Id ,eap) %>%
+    pivot_wider(names_from = Test_Id, values_from = eap ) %>%
+    na.omit() %>%
+    mutate(eap = glue("{round(`1`,1)}% in ELA and {round(`2`,1)}% in Math") ) %>%
+    select(cds,eap)
+
+
 
 
 ## Middle Drop out
@@ -304,21 +296,28 @@ cred_rate <- staff.mry %>%
 
 reclass <- tbl(con, "RECLASS") %>%
     filter(County =="Monterey",
-           YEAR == max(YEAR)) %>%
+ #          YEAR == max(YEAR)
+           ) %>%
     collect()    %>%
-group_by(District) %>%
+group_by(District, YEAR) %>%
     mutate(reclassified = sum(Reclass),
            ELenroll = sum(EL)) %>%
     ungroup() %>%
-    transmute(
-        reclass_rate = (reclassified*100/ELenroll) %>% round2(1), 
-        cds = paste0(str_sub(CDS,1,7),"0000000")  ) %>% 
-    distinct() 
+    mutate(  cds = paste0(str_sub(CDS,1,7),"0000000") ) %>%
+    select(YEAR, District, cds, reclassified, ELenroll) %>%
+    distinct() %>%
+    mutate(YEAR = case_when(YEAR == max(YEAR) ~ "new",
+                            YEAR ==  max( YEAR[YEAR!=max(YEAR)] ) ~ "new_minus_one",  # Reclass rate is calculated by RFEPs in a given year divided by total EL in previous year
+                            TRUE ~ "old"))  %>%
+    filter(YEAR != "old"  ) %>%
+    pivot_wider(values_from = c("reclassified", "ELenroll"), names_from = "YEAR") %>%
+    transmute(cds,
+        reclass_rate = (reclassified_new*100/ELenroll_new_minus_one) %>% round2(1) ) 
 
 ### Combine all the dashboard files ----
 
 indicators <- list(   susp,exp , math, ela, 
-                      A_G, # AP,
+                      A_G, eap, # AP,
                       chronic, elpi, grad, drop, reclass,
                       cred_rate) %>%
     reduce( left_join)
