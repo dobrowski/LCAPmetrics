95601c385117c3700efb0867bb3a073d240cfb8d
David Dobrowski <dobrowski@gmail.com>
2021-05-12 10:59:42 -0700

    Updating required metrics

--- LCAPmetrics2021.R
+++ LCAPmetrics2021.R
@@ -21,66 +21,76 @@ con <- mcoe_sql_con()
 
 ### Import data -------
 
-metrics <- tibble("priority_area" = c(rep("Conditions for Learning",5),rep("Pupil Outcomes",8), rep("Engagement",10) ) ,
-                  "priorities" = c(rep("1. Basic",3),"2. Implement State Standards","7. Course Access", rep("4. Pupil Achievement*", 7), "8. Other Pupil Outcomes", rep("3. Parent Involvement",2), rep("5. Pupil Engagement*", 5), rep("6. School Climate*",3) ),
-                  "metrics" = c("cred.rate.wt","Materials","GoodRepair","Standards","BroadCourse", #5
-                                "math", "ela" ,"ag_cte_perc","elpi","reclass_rate", #10
-                                "ap","eap","outcomes_other", "ParentInput","UnduplicatedParentPart", #15
-                                "Attendance","chronic","MSdropout","DropoutRate","grad", #20
+metrics <- tibble("priority_area" = c(rep("Conditions for Learning",8),rep("Pupil Outcomes",10), rep("Engagement",11) ) ,
+                  "priorities" = c(rep("1. Basic",3),rep("2. Implement State Standards",2),rep("7. Course Access",3), rep("4. Pupil Achievement*", 9), "8. Other Pupil Outcomes", rep("3. Parent Involvement",3), rep("5. Pupil Engagement*", 5), rep("6. School Climate*",3) ),
+                  "metrics" = c("cred.rate.wt","Materials","GoodRepair","Standards", "StandardsEL" , #5
+                                "BroadCourse", "CourseUndup", "CourseSWD" , #8
+                                "math", "ela" ,"ag_cte_perc", "ag_perc" , "cte_perc" ,"elpi","reclass_rate", #13
+                                "ap_perc","eap","outcomes_other", "ParentInput","UnduplicatedParentPart", "ParentSWD", #19
+                                "Attendance","chronic","MSdropout","DropoutRate","grad", #23
                                 "susp","exp","local_other"),
                   "source" = c(rep("Local Dashboard Data",1),
                                rep("Local Dashboard Data and  district Williams Report",2),
                                "LEA and site CCSS implementation plans and teacher participation in CCSS training.",
-                               "student information systems", #5
-                               rep("Dashboard",2),
-                               "Dashboard",
-                               "Dashboard",
-                               "Dataquest",# https://www.cde.ca.gov/ci/gs/hs/eapindex.asp  doesn't exist, maybe in CCI? #10
+                               "LEA and site CCSS and ELD implementation plans and teacher participation in CCSS and ELD training.", #5
+                               rep("student information systems", 3),
+                               rep("Dashboard",2),#10
+                               "",
+                               rep("Dashboard",3), 
+                               "Dataquest",#15 # https://www.cde.ca.gov/ci/gs/hs/eapindex.asp  doesn't exist, maybe in CCI? 
                                "Dashboard",
                                "CAASPP results",
                                "Local data",
                                "Local Dashboard Data and district and school surveys related to WASC and Single Plan Student Achievement, or DLAC/ELAC",
-                               "Local Dashboard Data and local data on parent involvement in district/school activities (e.g., committees, student clubs, after school enrichment, fundraisers, carnivals, promotion activities, PTO membership)",#15
+                               rep("Local Dashboard Data and local data on parent involvement in district/school activities (e.g., committees, student clubs, promotion activities, PTO membership, etc.)",2),#20
                                "student information systems",
                                "Dashboard",
                                rep("student information systems",2),
-                               "Dashboard", #20
+                               "Dashboard", #25
                                "Dataquest", 
                                "Dataquest", 
                                "Local Dashboard Data and from such sources as LEA plans, School Site Council activities, and English Learner Advisory Council materials"   ),
                   "description" = c("Teachers: Fully Credentialed & Appropriately Assigned", 
                                     "Standards-aligned Instructional Materials for every student",
                                     "School Facilities in “Good Repair” per CDE’s Facility Inspection Tool (FIT)",
-                                    "Implementation of all CA state standards including how ELs will access the CCSS and ELD standards", 
-                                    "Students have access and are enrolled in a broad course of study (Social Science, Health, VAPA, Science, PE, World Language)", #5
+                                    "Implementation of all CA state standards for all students", 
+                                    "Implmentation of how ELs will access the CCSS and ELD standards", #5
+                                    "Students have access and are enrolled in a broad course of study (Social Science, Health, VAPA, Science, PE, World Language)", 
+                                    "Students have access and are enrolled in programs and services developed and provided to low income, English learner and foster youth pupils",
+                                    "Students have access and are enrolled in programs and services developed and provided to students with disabilities.", #8
+                                    
                                     "State CAASPP assessments (Math DFS)", 
                                     "State CAASPP assessments (ELA DFS)", 
-                                    "Percent of pupils that have successfully completed A-G requirements or CTE pathways", 
+                                    "Percent of pupils that have successfully completed A-G courses and CTE courses", 
+                                    "Percent of pupils that have successfully completed A-G courses", 
+                                    "Percent of pupils that have successfully completed CTE courses", 
                                     "Percent of ELs who progress in English proficiency (ELPAC)",
-                                    "EL reclassification rate", #10
+                                    "EL reclassification rate", #13
                                     "Percent of pupils that pass AP exams with a score of 3 or higher",
                                     "Pupils prepared for college by the EAP (ELA/Math CAASPP Score of 3 or higher)",
                                     "If available, outcomes for subjects listed in course access" ,
                                     "Parent input in decision-making",
-                                    "Parental Participation in programs for Unduplicated Pupils (UPs)", #15
+                                    "Parental Participation in programs for Unduplicated Pupils (UPs)", #18
+                                    "Parental Participation in programs for Students with Disabilities", 
                                     "Attendance rates", 
                                     "Chronic absenteeism (CA) rates", 
                                     "Middle school dropout rates", 
                                     "High school dropout rates", 
-                                    "High school graduation rates", #20
+                                    "High school graduation rates", #24
                                     "Suspension rates", 
                                     "Expulsion rates", 
                                     "Other local measures (Surveys regarding safety and school connectedness)"
                   ),
                   "website" = c("https://www.cde.ca.gov/ds/sd/df/filesstaffdemo.asp",
-                                rep("",4), #5
+                                rep("",7), #5
                                 rep("Dashboard Datafiles at https://www.cde.ca.gov/ta/ac/cm/datafiles2019.asp",2),
-                                "Dashboard Datafiles at https://www.cde.ca.gov/ta/ac/cm/",
+                                "",
+                                rep("Dashboard Datafiles at https://www.cde.ca.gov/ta/ac/cm/",2),
                                 "Dashboard Datafiles at https://www.cde.ca.gov/ta/ac/cm/datafiles2019.asp",
                                 "https://www.cde.ca.gov/ds/sd/sd/filesreclass.asp", #10
                                 "Dashboard Datafiles at https://www.cde.ca.gov/ta/ac/cm/",
                                 "caaspp-elpac.cde.ca.gov",
-                                rep("",4), #16
+                                rep("",5), #16
                                 "Dashboard Datafiles at https://www.cde.ca.gov/ta/ac/cm/datafiles2019.asp",
                                 "",
                                 "https://www.cde.ca.gov/ds/sd/sd/filesacgr.asp",
@@ -90,18 +100,21 @@ metrics <- tibble("priority_area" = c(rep("Conditions for Learning",5),rep("Pupi
                                 rep("",1)
                   ),
                   "notes" = c( "Please note this only looks at teachers and not adminstrators, pupil services, itinerant nor push-in/pull-out teachers. The most recent available public data is 2018-19. It is grouped at the district level, and is weighted by percent FTE.  It does not yet look at proper assignment of teachers, only credential status. Please also reference your Williams Report."  ,
-                               rep("",4), #5
+                               rep("",7), #5
                                rep("Since this is from 2018-19, it is recommended to use local data, such as interims or NWEA, rather than data from two years ago.",2),
-                               "Note 2019-20 data is available on the Dashboard. Please note this is the best possible calculation, but will be slightly inflated for students that completed A-G and also complete a CTE pathway. It is not possible to get the exact figure with the aggregate data available.  CALPADS Report 3.14, 3.15 and 15.1 may be helpful.", # just use the CCI indicator too. 
+                               "It is not possible to calculate students that completed both A-G courses and also completed a CTE course with public data. Your SIS or CALPADS Report 3.14, 3.15 and 15.1 may be helpful.", # just use the CCI indicator too. 
+                               rep("Note 2019-20 data is available on the Dashboard. This is calculated using the number of students prepared and approaching prepared. ",2), # just use the CCI indicator too. 
                                rep("",2), #10  #  Look at Title III AMOA 2  reporting
                                "Note 2019-20 data is available on the Dashboard. Please note this is the percentage of the graduating cohort that passed TWO AP exams. The percentage of students that passed a single AP exam is not available on the Dashboard. Please also look at College Board Online https://scores.collegeboard.org/pawra/home.action",  #  Look at AP data file,  also add link to College Board Online https://scores.collegeboard.org/pawra/home.action
                                rep("",2),  #   https://www.cde.ca.gov/ci/gs/hs/eapindex.asp
-                               "One possible example is a rating on a self-assessment tool.",
-                               "Consider daily participation or weekly engagement records.", #15
                                "",
+                               "",
+                               "",
+                               "Consider daily participation or weekly engagement records.", #15
+
                                "CALPADS Report 14.1 and 14.2 may be helpful.", #17
                                "Please note LEAs can find middle school dropouts information from CALPADS snapshot report 1.14: Dropouts Count – State View (filtered for grades seven and eight)",
-                               "",
+                               "Please note LEAs can find high school dropouts information from CALPADS snapshot report 1.14: Dropouts Count – State View (filtered for grades nine through twelve)",
                                "Note 2019-20 data is available on the Dashboard.  CALPADS Report 15.1 and 15.2 may be helpful.", #20
                                "CALPADS Report 7.10 and 7.12 may be helpful.",
                                "Please note this is a rate per thousand students.",
@@ -176,12 +189,16 @@ elpi <- tbl(con, "DASH_ELPI") %>%
 A_G <- tbl(con, "DASH_CCI") %>%
     filter(studentgroup == "ALL",
            reportingyear == max(reportingyear)) %>%
-    mutate(ag_cte = curr_prep_agplus + curr_prep_cteplus + curr_aprep_ag + curr_aprep_cte,
-           ag_cte_perc = ag_cte*100/currdenom) %>%
-    select(cds:countyname,ag_cte_perc,curr_prep_apexam,currdenom) %>%
+    mutate(# ag_cte = curr_prep_agplus + curr_prep_cteplus + curr_aprep_ag + curr_aprep_cte,
+           # ag_cte_perc = ag_cte*100/currdenom ,
+           ag_perc = (curr_prep_agplus + curr_aprep_ag) / currdenom ,
+           cte_perc = (curr_prep_cteplus + curr_aprep_cte) / currdenom ,
+           ap_perc = (curr_prep_apexam ) / currdenom ,
+           )  %>%
+    select(cds:countyname, ends_with("perc")) %>%
     collect() %>%
-    filter(str_detect(countyname,"Monterey")) %>%
-    mutate(ap = (curr_prep_apexam/currdenom) %>% round2(3)*100)
+    filter(str_detect(countyname,"Monterey")) # %>%
+#    mutate(ap = (curr_prep_apexam/currdenom) %>% round2(3)*100)
 
 # Note, this is the best possible calculation, but will be slightly inflated for students that completed A-G and also complete a CTE pathway. 
 # It is not possible to get the exact figure with the aggregate data available
@@ -328,11 +345,14 @@ indicators <- indicators %>%
            cred.rate.wt = scales::percent( cred.rate.wt/100 , accuracy = .1 ),
            elpi = scales::percent( elpi/100 , accuracy = .1 ),
            reclass_rate = scales::percent( reclass_rate/100 , accuracy = .1 ),
-           ap = scales::percent( ap/100 , accuracy = .1 ),
+           ap_perc = scales::percent( ap_perc , accuracy = .1 ),
+           ag_perc = scales::percent( ag_perc , accuracy = .1 ),
+           cte_perc = scales::percent( cte_perc , accuracy = .1 ),
            chronic = scales::percent( chronic/100 , accuracy = .1 ),
            grad = scales::percent( grad/100 , accuracy = .1 ),
            Dropout_Rate = scales::percent( Dropout_Rate/100 , accuracy = .1 ),
-           ag_cte_perc = scales::percent( ag_cte_perc/100 , accuracy = .1 )
+  #         ag_cte_perc = scales::percent( ag_cte_perc/100 , accuracy = .1 )
+           
            
            )
 
