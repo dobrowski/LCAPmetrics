40591a22860991a258e328521018dbeac5bcba88
David Dobrowski <dobrowski@gmail.com>
2024-03-05 14:41:52 -0800

    Update analytics to track which districts pulled

--- ui.R
+++ ui.R
@@ -31,9 +31,7 @@ shinyUI(fluidPage(
         sidebarPanel(
             selectInput("select", label = h3("Select box"),
                         choices = series_ids[order(names(series_ids))],
-                #        choices = list( "Alisal" = "27659610000000","Big Sur" = "27751500000000" ,"Bradley" = "27659790000000", "Chualar" = "27659950000000", "North Monterey County" = "27738250000000", "Salinas Union" = "27661590000000", "South Monterey" = "27660680000000", "Washington" = "27662330000000"), 
                         selected = 1),
-     #       sliderInput("slider", "Slider", 27661590000000, 27661590000000, 1),
             actionButton("generate", "Generate Report", icon = icon("file"), # This is the only button that shows up when the app is loaded
                   style = "color: #fff; background-color: #337ab7; border-color: #2e6da4"),
      br(),
